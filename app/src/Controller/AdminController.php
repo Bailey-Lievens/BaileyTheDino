f88c8cb996644d99c33e44b8cc843e9669039dee
bailey lievens <baileylievens@hotmail.be>
2023-02-11 12:39:24 +0100

    Feat: Added admin navigation

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -16,4 +16,34 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig');
     }
+
+    #[Route('/project', name: 'backend_project')]
+    public function project(): Response
+    {
+        return $this->render('admin/project/index.html.twig');
+    }
+
+    #[Route('/blog', name: 'backend_blog')]
+    public function blog(): Response
+    {
+        return $this->render('admin/blog/index.html.twig');
+    }
+
+    #[Route('/quote', name: 'backend_quote')]
+    public function quote(): Response
+    {
+        return $this->render('admin/quote/index.html.twig');
+    }
+
+    #[Route('/category', name: 'backend_category')]
+    public function category(): Response
+    {
+        return $this->render('admin/category/index.html.twig');
+    }
+
+    #[Route('/account', name: 'backend_account')]
+    public function account(): Response
+    {
+        return $this->render('admin/account/index.html.twig');
+    }
 }
