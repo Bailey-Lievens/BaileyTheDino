aa23b63e0591980cd88c9e79f0884e63ae20f67e
BaileyTheDino <baileylievens@hotmail.be>
2023-10-18 22:19:55 +0200

    Fixed icons being blurry

--- translations/messages.en.php
+++ translations/messages.en.php
@@ -10,6 +10,18 @@ return [
                 'blog' => 'Blog',
                 'contact' => 'Contact',
             ]
-        ]
+        ],
+        'index' => [
+            'pageTitle' => 'BaileyTheDino'
+        ],
+        'about' => [
+            'pageTitle' => 'About'
+        ],
+        'work' => [
+            'pageTitle' => 'Work'
+        ],
+        'blog' => [
+            'pageTitle' => 'Blog'
+        ],
     ],
 ];
