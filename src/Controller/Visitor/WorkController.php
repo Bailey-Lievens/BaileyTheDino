b69e4f6f995b534b2af6dc515f28c6371e2e373d
BaileyTheDino <baileylievens@hotmail.be>
2023-10-19 22:20:54 +0200

    Added work loader

--- src/Controller/Visitor/WorkController.php
+++ src/Controller/Visitor/WorkController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller\Visitor;
 
+use App\Loader\WorkLoader;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class WorkController extends AbstractController
 {
+    public function __construct(
+        private readonly WorkLoader $workLoader
+    )
+    {
+    }
+
     #[Route('/work', name: 'visitor.work.listing')]
     public function work(): Response
     {
-        return $this->render('visitor/index/work/index.html.twig');
+        $workCollections = $this->workLoader->getWorkCollections();
+
+        return $this->render('visitor/index/work/index.html.twig', [
+            'page' => [
+                'workCollections' => $workCollections
+            ]
+        ]);
     }
 }
