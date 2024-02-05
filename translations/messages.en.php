e3ee5a2abe146e43ad35d8ea9ec2ee5077a82a07
BaileyTheDino <baileylievens@hotmail.be>
2024-02-05 22:25:35 +0100

    Feat: Added some more blocks

--- translations/messages.en.php
+++ translations/messages.en.php
@@ -12,6 +12,25 @@ return [
                 'subText' => 'In collaboration with Zoro',
                 'scrollDown' => 'Head on down'
             ],
+            'middleText' => [
+                'text' => 'A place I can info dump and rant to my hearts content'
+            ],
+            'contentCta' => [
+                'title' => 'Content such as',
+                'links' => [
+                    'link-1' => '✨Opinions✨',
+                    'link-2' => 'Zoro\'s life',
+                    'link-3' => 'Fun facts',
+                    'link-4' => 'Anime',
+                    'link-5' => 'Not so fun facts',
+                    'link-6' => 'Coding things',
+                ],
+                'footNote' => 'Damn that\'s cool',
+            ],
+            'projectShowcase' => [
+                'title' => 'Some fan favorites',
+                'subTitle' => 'The fan being my cat, Zoro 🐈‍⬛'
+            ],
         ],
     ],
 ];
